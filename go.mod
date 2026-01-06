c151e7eadf1a638a5232963b4a89817203a26a63
AnlongZhou <anlongzhou96403@gmail.com>
2026-01-06 20:15:52 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module book-searching
+
+go 1.25.1
+
+require (
+	github.com/go-ego/gse v1.0.0 // indirect
+	github.com/vcaesar/cedar v0.20.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/go-ego/gse"
+)
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func newSegmenter() gse.Segmenter {
+
+	var segmenter gse.Segmenter
+	err := segmenter.LoadDict()
+
+	checkError(err)
+
+	return segmenter
+
+}
+
+func splitLine(line string) []string {
+
+	line = strings.ReplaceAll(line, "？", "。")
+	line = strings.ReplaceAll(line, "！", "。")
+	line = strings.ReplaceAll(line, "，", "。")
+	line = strings.ReplaceAll(line, "?", "。")
+	line = strings.ReplaceAll(line, "!", "。")
+
+	sentences := strings.Split(line, "。")
+	var clearSentences []string
+
+	for _, s := range sentences {
+		if strings.TrimSpace(s) != "" {
+			clearSentences = append(clearSentences, s)
+		}
+	}
+
+	return clearSentences
+}
+
+type Data struct {
+	wordIndex map[string][]int
+	index     int
+}
+
+func newWordIndex() map[string][]int {
+	return make(map[string][]int)
+}
+
+func newData() Data {
+	return Data{
+		wordIndex: newWordIndex(),
+		index:     0,
+	}
+}
+
+func printTable(data Data) {
+	for word, indices := range data.wordIndex {
+		fmt.Printf("%s appears in: %d\n", word, indices)
+	}
+}
+
+func main() {
+	infile, err := os.Open("input.txt")
+	checkError(err)
+
+	segmenter := newSegmenter()
+	reader := bufio.NewReader(infile)
+
+	data := newData()
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		sentences := splitLine(line)
+
+		for _, sentence := range sentences {
+
+			words := segmenter.Cut(sentence)
+
+			for _, word := range words {
+				data.index += 1
+				data.wordIndex[word] = append(data.wordIndex[word], data.index)
+			}
+		}
+
+		printTable(data)
+
+		fmt.Println("Finished")
+
+	}
+}
